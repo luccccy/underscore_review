3a898c07d33fb3e0e5cdc16dfd7781ce0c612c67
Andy Fang <afang1094@gmail.com>
2021-08-03 18:19:19 -0700

    updated all test functions up to the shuffle section

--- spec/part2.js
+++ spec/part2.js
@@ -195,7 +195,11 @@
       it('should override properties found on the destination', function() {
         // Replace this line with an `expect` statement that tests
         // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        var destination = { x: 'x' };
+        var source = { x: 'b' };
+        var extended = _.extend(destination, source);
+
+        expect(extended.x).to.equal('b');
       });
 
       it('should not override properties not found in the source', function() {
@@ -273,7 +277,12 @@
       it('should copy any property whose key is not already set on the target', function() {
         // Replace this line with an `expect` statement that tests
         // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        var destination = {};
+        var source = { a: 10, b: 1 };
+
+        _.defaults(destination, source);
+
+        expect(destination.a).to.equal(10);
       });
 
       it('should not copy a property if that key is already set on the target', function() {
